29c0e6a1206d075c7ed16f26b0896ae62a73f746
andym@ANDYMULLER <andym@ANDYMULLER>
2019-05-28 18:52:05 -0500

    Update app.js

--- app.js
+++ app.js
@@ -10,9 +10,18 @@ var insultsHealthy = {
     lowSugar: "You get scared on Halloween, and it isn't because of the ghosts.",
     lowSodium: "You literally have no taste.",
 }
-var searchTerm = ;
+var searchTerm = $(this).attr("data-search");
 var insultAndyMuller = "Andy, nobody cares about what you eat in class.";
 var pixabayAPI = "12579168-7c66fd8723598ff87605657fc";
 var nutritionixAPI = "43a4724158ea08c8e5b9070c4f8ecd5f";
 var imageQuery = "https://pixabay.com/api/?key=" + pixabayAPI + searchTerm + "&image_type=photo";
 var nutritionQuery = "https://api.nutritionix.com/v1_1/search/" + searchTerm + "?results=0%3A01&cal_min=0&cal_max=50000&fields=*&appId=c3659b4f&appKey=" + nutritionixAPI;
+
+function displayNutrition(){
+    $.ajax({
+        url: nutritionQuery,
+        method: "GET"
+    }).then(function(response){
+        console.log(response);
+    })
+}
