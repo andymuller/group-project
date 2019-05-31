0ee249d5c8bdc942fbe060eb3fd1285c0abb9b7c
andymuller <49294608+andymuller@users.noreply.github.com>
2019-05-30 19:56:54 -0500

    Update app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -48,6 +48,7 @@ var nutritionQuery = "https://api.nutritionix.com/v1_1/search/"+food+"?results=0
 }
 $("#search-button").click(function(event){
     event.preventDefault();
+    $(".inner-cover").animate({ top: "-= 200"}, "normal");
     var searchTerm = $("#food-search").val().trim();
     displayNutrition(searchTerm);
 })
